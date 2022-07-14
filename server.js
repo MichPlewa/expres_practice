e24ddcbbac687aac6d3167b080acaadd8ec78484
Michal Plewa <themipsial@gmail.com>
2022-07-14 21:28:18 +0200

    edit all endpoints

--- server.js
+++ server.js
@@ -1,16 +1,51 @@
 const express = require('express');
 const path = require('path');
+const uuid = require('uuid').v4;
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/public')));
-
 app.get('/testimonials', (req, res) => {
   res.send(db);
+  res.json({ message: 'Ok' });
 });
 
-app.get('/testimonial/:id', (req, res) => {
-  res.send(findById(parseInt(req.params.id), db));
+app.get('/testimonials/:id', (req, res) => {
+  const id = req.params.id;
+  if (id === 'random') {
+    res.send(db[Math.floor(Math.random() * db.length)]);
+    res.json({ message: 'Ok' });
+  } else {
+    res.send(db.find((item) => item.id === id - 1));
+    res.json({ message: 'Ok' });
+  }
+});
+
+app.post('/testimonials', (req, res) => {
+  const { author, text } = req.body;
+  const id = uuid();
+  const newTestimonial = { id: id, author, text };
+  db.push(newTestimonial);
+  res.json({ message: 'Ok' });
+});
+
+app.put('/testimonials/:id', (req, res) => {
+  const { author, text } = req.body;
+  const id = req.params.id;
+  const testimonials = db.find((item) => item.id === id);
+  testimonials.author = author;
+  testimonials.text = text;
+  res.json({ message: 'Ok' });
+});
+
+app.delete('/testimonials/:id', (res, req) => {
+  const id = res.params.id;
+  db.splice(db.findIndex((item) => item.id === id));
+  res.json({ message: 'Ok' });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'error' });
 });
 
 app.listen(8000, () => {
@@ -25,5 +60,3 @@ const db = [
     text: 'They really know how to make you happy.',
   },
 ];
-
-const findById = (id, data) => data.find((item) => item.id === id);
