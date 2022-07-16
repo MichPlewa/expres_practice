dbec18aeb6d84697aaf2e21c9d4dd41dce63986a
Michal Plewa <themipsial@gmail.com>
2022-07-16 13:11:32 +0200

    fix endpoint

--- routes/testimonials.routes.js
+++ routes/testimonials.routes.js
@@ -1,8 +1,7 @@
 const express = require('express');
+const router = express.Router();
 const { testimonials } = require('../db');
 const uuid = require('uuid').v4;
-
-const router = express.Router();
 
 router.route('/testimonials').get((req, res) => {
   res.json(testimonials);
--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
 
 const app = express();
 
-const testimonialsRoutes = require('./routes/testimonials.routes');
-const concertsRoutes = require('./routes/concerts.routes');
-const seatsRoutes = require('./routes/seats.routes');
+const testimonialsRoutes = require('./routes/testimonials.routes.js');
+const concertsRoutes = require('./routes/concerts.routes.js');
+const seatsRoutes = require('./routes/seats.routes.js');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/api', testimonialsRoutes);
